2d363199a0adab9e9cfc27cc7780132809fa1f02
work-crosca <work.crosca@gmail.com>
2025-12-31 10:58:32 +0200

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "./LanguageSwitcher";
 import "./Navbar.css";
 
+import logo from "../assets/icons/logo.svg";
+
 const Navbar: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <header className="navRoot">
       <div className="navInner">
-        <div className="navRight">
-          <LanguageSwitcher />
+        <div className="navGrid">
+          {/* Coloana 1 – Logo */}
+          <div className="navCol navLogoCol">
+            <a href="/" className="navLogoLink" aria-label={t("tabTitleEU")}>
+              <img src={logo} alt="Logo" className="navLogo" />
+            </a>
+          </div>
+
+          {/* Coloana 2 – Language Switcher */}
+          <div className="navCol navSwitcherCol">
+            <LanguageSwitcher />
+          </div>
         </div>
       </div>
     </header>
